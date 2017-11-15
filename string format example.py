3cb95f75f61fa450b467e9a610494b7c35a1609d
hailulai <hailu0731@gmail.com>
2017-11-15 16:06:11 -0600

    string format
    string format example

---
+++ string format example.py
@@ -0,0 +1,21 @@
+## this function gives an example for how to use string format
+## Nov. 15
+
+def pushUps(numbers):
+    for numbers in range(1, numbers):
+        if numbers == 1:
+                print ("I did push-ups for the {num}st time".format(num =
+                                                                    numbers))
+        elif numbers == 2:
+            	print ("I did push-ups for the {num}nd time".format(num =
+                                                                    numbers))
+        elif numbers ==3:
+            	print ("I did push-ups for the {num}rd time".format(num =
+                                                                    numbers))
+        else:
+            	print ("I did push-ups for the {num}th time".format(num =
+                                                                    numbers))
+
+
+
+            
